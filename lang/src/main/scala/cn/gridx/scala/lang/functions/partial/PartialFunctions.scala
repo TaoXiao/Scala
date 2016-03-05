0f83e8ba8f659e0b0e89744313c33557e0c7220f
tao@gridx.com <tao@gridx.com>
2016-03-05 14:35:32 +0800

    增加Akka中 ? 和 Future 的使用

---
+++ lang/src/main/scala/cn/gridx/scala/lang/functions/partial/PartialFunctions.scala
@@ -0,0 +1,19 @@
+package cn.gridx.scala.lang.functions.partial
+
+/**
+  * Created by tao on 3/4/16.
+  */
+object PartialFunctions {
+
+  def main(args: Array[String]): Unit = {
+    //f1("100")
+    //f1(200f)
+    //f1('x')
+  }
+
+  def f1 = {
+    case x: String => println(s"String: $x ")
+    case x: Float  => println(s"Float: $x")
+    case _         => println("Unknown type and value")
+  }
+}
