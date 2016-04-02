8857b5d8e6374fb9a5a6d20430d5c928ce8b5b2d
tao@gridx.com <tao@gridx.com>
2016-04-02 14:25:21 +0800

    增加对数据的操作：写入、读取、删除数据（针对Netflix Priam所需的domain）

--- aws/src/main/scala/cn/gridx/scala/aws/simpledb/DataOps.scala
+++ aws/src/main/scala/cn/gridx/scala/aws/simpledb/DataOps.scala
@@ -13,10 +13,11 @@ import com.amazonaws.services.simpledb.model._
 object DataOps {
 
   def CassCluster = "dseCassandraCluster"
+
   def PriamPropertiesDomain = "PriamProperties"
 
   def main(args: Array[String]): Unit = {
-    def AccessKey  = args(0)
+    def AccessKey = args(0)
     def SecreteKey = args(1)
 
     val dbClient = new AmazonSimpleDBClient(new BasicAWSCredentials(AccessKey, SecreteKey))
@@ -24,18 +25,19 @@ object DataOps {
 
     // PutData(dbClient)
     // ReadData(dbClient)
-    // DeleteData(dbClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.home")
+    // DeletePriamAttrs(dbClient)
     // WritePriamAttrs(dbClient)
-    // ReadPriamAttrs(dbClient)
+    ReadPriamAttrs(dbClient)
   }
 
-  /***
+  /** *
     * 写入一些数据
+    *
     * @param dbClient
     */
   def PutData(dbClient: AmazonSimpleDBClient, domain: String, item: String): Unit = {
     println("开始写入数据")
-    val attrs =  new java.util.ArrayList[ReplaceableAttribute]()
+    val attrs = new java.util.ArrayList[ReplaceableAttribute]()
     attrs.add(new ReplaceableAttribute("N1", "V1", false))
     attrs.add(new ReplaceableAttribute("N2", "V2", false))
 
@@ -44,8 +46,9 @@ object DataOps {
   }
 
 
-  /***
+  /** *
     * 读取数据
+    *
     * @param dbClient
     */
   def ReadData(dbClient: AmazonSimpleDBClient, domain: String, item: String) = {
@@ -71,84 +74,116 @@ object DataOps {
 
   /**
     * 删除一些数据
+    *
     * @param dbClient
     */
   def DeleteData(dbClient: AmazonSimpleDBClient, domain: String, item: String): Unit = {
-    println(s"正在删除数据: domain -> $domain,  item -> $item")
     dbClient.deleteAttributes(new DeleteAttributesRequest(domain, item))
-    println("删除数据完毕")
   }
 
 
-  /***
+  /** *
     * 写入Netflix Priam所需的属性
+    *
     * @param dbClient
     */
   def WritePriamAttrs(dbClient: AmazonSimpleDBClient): Unit = {
+    def AppId = "ASG_CassCluster"
+
     println("正在查询存在的domains ... ")
     val domainIt = dbClient.listDomains.getDomainNames.iterator
     while (domainIt.hasNext)
       println(domainIt.next)
     println("domain查询完毕...")
 
-    var attrs =  new java.util.ArrayList[ReplaceableAttribute]()
-    attrs.add(new ReplaceableAttribute("appId", CassCluster, false))
+    var attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
     attrs.add(new ReplaceableAttribute("property", "priam.clustername", false))
-    attrs.add(new ReplaceableAttribute("value" , CassCluster, false))
+    attrs.add(new ReplaceableAttribute("value", CassCluster, false))
     dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.clustername", attrs))
 
 
-    attrs =  new java.util.ArrayList[ReplaceableAttribute]()
-    attrs.add(new ReplaceableAttribute("appId", CassCluster, false))
+    attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
     attrs.add(new ReplaceableAttribute("property", "priam.cass.home", false))
-    attrs.add(new ReplaceableAttribute("value" , "/usr/share/dse/cassandra", false))
+    attrs.add(new ReplaceableAttribute("value", "/usr/share/dse/cassandra", false))
     dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.cass.home", attrs))
 
-    attrs =  new java.util.ArrayList[ReplaceableAttribute]()
-    attrs.add(new ReplaceableAttribute("appId", CassCluster, false))
+    attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
     attrs.add(new ReplaceableAttribute("property", "priam.cass.startscript", false))
-    attrs.add(new ReplaceableAttribute("value" , "/etc/init.d/dse start", false))
+    attrs.add(new ReplaceableAttribute("value", "sudo /etc/init.d/dse start", false))
     dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.cass.startscript", attrs))
 
-    attrs =  new java.util.ArrayList[ReplaceableAttribute]()
-    attrs.add(new ReplaceableAttribute("appId", CassCluster, false))
+    attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
     attrs.add(new ReplaceableAttribute("property", "priam.cass.stopscript", false))
-    attrs.add(new ReplaceableAttribute("value" , "/etc/init.d/dse stop", false))
+    attrs.add(new ReplaceableAttribute("value", "sudo /etc/init.d/dse stop", false))
     dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.cass.stopscript", attrs))
 
-    attrs =  new java.util.ArrayList[ReplaceableAttribute]()
-    attrs.add(new ReplaceableAttribute("appId", CassCluster, false))
+    attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
     attrs.add(new ReplaceableAttribute("property", "priam.data.location", false))
-    attrs.add(new ReplaceableAttribute("value" , "/data/cassandra/data", false))
+    attrs.add(new ReplaceableAttribute("value", "/data/cassandra/data", false))
     dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.data.location", attrs))
 
-    attrs =  new java.util.ArrayList[ReplaceableAttribute]()
-    attrs.add(new ReplaceableAttribute("appId", CassCluster, false))
+    attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
     attrs.add(new ReplaceableAttribute("property", "priam.cache.location", false))
-    attrs.add(new ReplaceableAttribute("value" , "/data/cassandra/saved_caches", false))
+    attrs.add(new ReplaceableAttribute("value", "/data/cassandra/saved_caches", false))
     dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.cache.location", attrs))
 
-    attrs =  new java.util.ArrayList[ReplaceableAttribute]()
-    attrs.add(new ReplaceableAttribute("appId", CassCluster, false))
+    attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
     attrs.add(new ReplaceableAttribute("property", "priam.commitlog.location", false))
-    attrs.add(new ReplaceableAttribute("value" , "/data/cassandra/commitlog", false))
+    attrs.add(new ReplaceableAttribute("value", "/data/cassandra/commitlog", false))
     dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.commitlog.location", attrs))
 
-    attrs =  new java.util.ArrayList[ReplaceableAttribute]()
-    attrs.add(new ReplaceableAttribute("appId", CassCluster, false))
+    attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
     attrs.add(new ReplaceableAttribute("property", "priam.s3.bucket", false))
-    attrs.add(new ReplaceableAttribute("value" , "gridx-cassandra-archive", false))
+    attrs.add(new ReplaceableAttribute("value", "gridx-cassandra-archive", false))
     dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.s3.bucket", attrs))
+
+    attrs = new java.util.ArrayList[ReplaceableAttribute]()
+    attrs.add(new ReplaceableAttribute("appId", AppId, false))
+    attrs.add(new ReplaceableAttribute("property", "priam.yamlLocation", false))
+    attrs.add(new ReplaceableAttribute("value", "/etc/dse/cassandra/cassandra.yaml", false))
+    dbClient.putAttributes(new PutAttributesRequest(PriamPropertiesDomain, s"${CassCluster}.priam.yamlLocation", attrs))
   }
 
+
+  /**
+    * 查看 `PriamProperties` domain 中的相关配置
+    **/
   def ReadPriamAttrs(dBClient: AmazonSimpleDBClient): Unit = {
-    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.clustername")
-    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.home")
-    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.startscript")
-    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.stopscript")
-    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.data.location")
-    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cache.location")
+    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.clustername"       )
+    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.home"         )
+    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.startscript"  )
+    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.stopscript"   )
+    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.data.location"     )
+    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cache.location"    )
     ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.commitlog.location")
-    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.s3.bucket")
+    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.s3.bucket"         )
+    ReadData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.yamlLocation"      )
+  }
+
+  def DeletePriamAttrs(dBClient: AmazonSimpleDBClient): Unit = {
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.clustername"       )
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.home"         )
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.startscript"  )
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cass.stopscript"   )
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.data.location"     )
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.cache.location"    )
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.commitlog.location")
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.s3.bucket"         )
+    DeleteData(dBClient, PriamPropertiesDomain, s"${CassCluster}.priam.yamlLocation"      )
+  }
+
+  /** *
+    * 查看 `InstanceIdentity` domaim 中的相关配置
+    */
+  def ReadInstanceIdentity(dBClient: AmazonSimpleDBClient): Unit = {
+
   }
 }
