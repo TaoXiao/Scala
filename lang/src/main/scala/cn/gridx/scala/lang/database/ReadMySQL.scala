983b763bd7349e0ca77140735744bada1c3afa91
tao@gridx.com <tao@gridx.com>
2016-06-17 09:46:57 +0800

    从MYSQL读取数据并保存在本地文件

--- lang/src/main/scala/cn/gridx/scala/lang/database/ReadMySQL.scala
+++ lang/src/main/scala/cn/gridx/scala/lang/database/ReadMySQL.scala
@@ -1,26 +1,85 @@
 package cn.gridx.scala.lang.database
 
+import java.io.PrintWriter
 import java.sql.DriverManager
+
+import scala.collection.mutable
 
 /**
   * Created by tao on 5/25/16.
   */
 object ReadMySQL {
+  def path = "data_1.txt"
   def main(args: Array[String]): Unit = {
-    var list = List[Long]()
-    list = 100L :: list
-    list = 200L :: list
-    list = 300L :: list
+    val url = "jdbc:mysql://10.100.34.151:3306/pge_prod?user=demo&password=RE3u6pc8ZYx1c"
+    val conn = DriverManager.getConnection(url)
+    var sql = "select count(*) from pge_prod.accountbillimpactmailer "
 
-    val x = list.forall(_ != 0)
-    println(x)
+    var pstmt = conn.prepareStatement(sql)
+    var rs = pstmt.executeQuery()
+    rs.next()
+    val total = rs.getInt(1)  //  总数
+    println(s"数据库中的记录总数为 $total")
+    rs.close()
 
-    /*
-    val conn = DriverManager.getConnection("jdbc:mysql://10.100.34.151/pge_prod")
-    val pstmt = conn.prepareStatement("select count(*) from customerbillingcycle")
-    val rs = pstmt.executeQuery
-    rs.next
-    println("结果为 " + rs.getInt(1))
-    */
+    val writer = new PrintWriter(path)
+
+    sql =
+      s""" select contract_id, servicepoint_id,
+          da, cca, tbs, care, fera, sr, sc, mb, ee, nems,
+          ratecompared1, ratecompared1_amt,
+          ratecompared2, ratecompared2_amt,
+          ratecompared3, ratecompared3_amt
+      from pge_prod.accountbillimpactmailer """
+
+    pstmt = conn.prepareStatement(sql)
+    rs = pstmt.executeQuery()
+    var count = 0
+    while (rs.next()) {
+      val key = rs.getString("contract_id") + ":" + rs.getString("servicepoint_id")
+
+      // 只导出非空的字段
+      val options = mutable.HashSet[String]()
+      if (!rs.getString("da").isEmpty)  options.add(s"""opt:da:${rs.getString("da")}""")
+      else  options.add(s"""opt:da:N""")
+
+      if (!rs.getString("cca").isEmpty)   options.add(s"""opt:cca:${rs.getString("cca")}""")
+      else  options.add(s"""opt:cca:N""")
+
+      if (!rs.getString("tbs").isEmpty)   options.add(s"""opt:tbs:${rs.getString("tbs")}""")
+      else  options.add(s"""opt:tbs:N""")
+
+      if (!rs.getString("care").isEmpty)  options.add(s"""opt:care:${rs.getString("care")}""")
+      else  options.add(s"""opt:care:N""")
+
+      if (!rs.getString("fera").isEmpty)  options.add(s"""opt:fera:${rs.getString("fera")}""")
+      else  options.add(s"""opt:fera:N""")
+
+      if (!rs.getString("sr").isEmpty)    options.add(s"""opt:sr:${rs.getString("sr")}""")
+      if (!rs.getString("sc").isEmpty)    options.add(s"""opt:sc:${rs.getString("sc")}""")
+      if (!rs.getString("mb").isEmpty)    options.add(s"""opt:mb:${rs.getString("mb")}""")
+      if (!rs.getString("ee").isEmpty)    options.add(s"""opt:ee:${rs.getString("ee")}""")
+      if (!rs.getString("nems").isEmpty)  options.add(s"""opt:nems:${rs.getString("nems")}""")
+
+      val dims = mutable.HashSet[String]()
+      if (!rs.getString("ratecompared1").isEmpty && !rs.getString("ratecompared1_amt").isEmpty)
+        dims.add(s"""dim:${rs.getString("ratecompared1")}:${rs.getString("ratecompared1_amt")}""")
+      if (!rs.getString("ratecompared2").isEmpty && !rs.getString("ratecompared2_amt").isEmpty)
+        dims.add(s"""dim:${rs.getString("ratecompared2")}:${rs.getString("ratecompared2_amt")}""")
+      if (!rs.getString("ratecompared3").isEmpty && !rs.getString("ratecompared3_amt").isEmpty)
+        dims.add(s"""dim:${rs.getString("ratecompared3")}:${rs.getString("ratecompared3_amt")}""")
+
+
+
+      writer.println(s"""$key,${options.mkString(",")},${dims.mkString(",")}""")
+      count += 1
+
+      if (count % 10000 == 0)
+        println(count)
+    }
+
+    writer.close()
+
+    conn.close()
   }
 }
