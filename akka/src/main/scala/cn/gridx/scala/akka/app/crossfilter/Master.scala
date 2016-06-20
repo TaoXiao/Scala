686ecb3d548508f7d9e6f38dc57c4e8ea9f4c9f3
tao@gridx.com <tao@gridx.com>
2016-06-20 13:47:43 +0800

    基于Akka的分布式cross filter core framework

--- akka/src/main/scala/cn/gridx/scala/akka/app/crossfilter/Master.scala
+++ akka/src/main/scala/cn/gridx/scala/akka/app/crossfilter/Master.scala
@@ -1,31 +1,107 @@
 package cn.gridx.scala.akka.app.crossfilter
 
-import akka.actor.{Actor, Props}
-import akka.routing.{RoundRobinRouter}
+import java.util
+
+import akka.actor.{Actor, ActorPath, ActorRef, ActorSystem, Props, RootActorPath}
+import com.typesafe.config.ConfigFactory
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 /**
-  * Created by tao on 6/17/16.
+  * Created by tao on 6/19/16.
   */
-class Master extends Actor {
-  var workerNum: Int = _
-  val workerLog = mutable.HashSet[Int]()
-  var ts_loadStart: Long = _
+class Master(dataPath: String, lineCount: Int, workingActors: Int) extends Actor {
+  // 存储所有的worker actors
+  val workers = ArrayBuffer[ActorRef]()
+  var numLoadingFinished = 0  //  多少个worker加载完成了
+  var numAnalysisFinished = 0
+  var ts_startLoading: Long = _
+  var ts_startAnalysis: Long = _
 
-  def receive = {
-    case MSG_START(config) =>
-      scheduleWorkers(config.path, assignData(config.path, config.lines, config.workers))
-    case MSG_WORKER_LOAD_FINISH(workerIdx, elapsed) =>
-      onLoadFinished(workerIdx, elapsed)
+
+  override def receive = {
+    case MSG_REGISTER_WORKER =>
+      registerWorker(sender())
+
+    case MSG_WORKER_LOAD_FINISH(elapsed) =>
+      onLoadFinished(sender(), elapsed)
+
+    case MSG_WORKER_ANALYSIS_FINISH(actorPath, elapsed, dimDistribution, optDistributions) =>
+      onAnalysisFinished(actorPath, elapsed, dimDistribution, optDistributions)
 
   }
+
+
+  /**
+    * 注册一个worker actor
+    * */
+  def registerWorker(worker: ActorRef): Unit = {
+    println(s"\n收到了消息[MSG_REGISTER_WORKER], sender = ${worker.path}\n")
+    if (workers.size < workingActors)
+      workers.append(worker)
+    else
+      println("注册workers的名额已满, 不再接受worker注册")
+
+    if (workers.size == workingActors) {
+      println(s"已经注册了${workers.size}个worker actors, 开始让各个worker加载数据")
+      loadData()
+    }
+  }
+
+
+  /**
+    * 指令各个workers去加载自己的数据
+    * */
+  def loadData(): Unit = {
+    ts_startLoading = System.currentTimeMillis()
+    val assignments = assignData(dataPath, lineCount, workers.size)
+    for (i <- 0 until workers.size) {
+      val (start, range) = assignments.get(i).get
+      workers(i) ! MSG_MASTER_LOAD(dataPath, start, range)
+    }
+  }
+
+
+  /**
+    * 当某个worker加载完自己的数据之后
+    * */
+  def onLoadFinished(actor: ActorRef, elapsed: Long): Unit = {
+    numLoadingFinished += 1
+    println(s"\nActor [${actor.path}] 完成了数据加载, 耗时 ${elapsed/1000} 秒\n")
+
+    // 当全部的workers记载完毕后, 开始分析数据
+    if (numLoadingFinished == workers.size) {
+      val totalElapsed = System.currentTimeMillis() - ts_startLoading
+      println(s"\n全部workers完成了数据加载, 整体耗时 ${totalElapsed/1000} 秒, 下面开始分析数据\n")
+
+      // 指令各个workers开始分析自己的数据
+      ts_startAnalysis = System.currentTimeMillis()
+      analyze()
+    }
+  }
+
+
+  def onAnalysisFinished(actorPath: ActorPath, elapsed: Long,
+                         dimDistribution: util.HashMap[String, util.TreeMap[Int, Int]],
+                         optDistributions: util.HashMap[String, util.HashMap[String, Int]]): Unit = {
+    println(s"Worker[${RootActorPath(actorPath.address)}] 完成计算, 耗时 ${elapsed/1000} 秒, 计算结果为: \n" +
+      s"dimDistribution = $dimDistribution\noptDistributions = $optDistributions\n")
+
+    numAnalysisFinished += 1
+    if (numAnalysisFinished == workers.size) {
+      val totalElapsed = System.currentTimeMillis() - ts_startAnalysis
+      println(s"\n全部的worker分析完成, 总体耗时 ${totalElapsed/1000} 秒\n")
+    }
+
+  }
+
+
 
   /**
     * 为每个worker分配任务(要计算的数据范围)
     * */
   def assignData(path: String, lines: Int, workers: Int): mutable.HashMap[Int, (Int, Int)] = {
-    workerNum = workers
     val x = lines / workers // 每个worker要分到多少行数据
     val y = lines % workers // 最后一个worker可能会多分到几行数据
 
@@ -36,7 +112,7 @@ class Master extends Actor {
       assignment.put(i, (x*i, x))
     assignment.put(workers - 1, (x*(workers - 1), x + y))
 
-    println("各个worker数据分配的情况是: ")
+    println("\n各个worker数据分配的情况是: ")
     for ((workerIdx, (start, range)) <- assignment)
       println(s"\tworker $workerIdx: ($start, $range) ")
 
@@ -45,27 +121,48 @@ class Master extends Actor {
 
 
   /**
-    * 开始调度workers
-    *
+    * 指令各个workers分析自己的数据
     * */
-  def scheduleWorkers(path: String, assignment: mutable.HashMap[Int, (Int, Int)]): Unit = {
-    val numWorkers = assignment.size
-    val router = context.actorOf(
-      Props[Worker].withRouter(RoundRobinRouter(numWorkers)), "Worker-Router")
+  def analyze(): Unit = {
+    // options的限制条件
+    val optFilter =  mutable.HashMap[String, String]()
+    optFilter.put("care", "Y")
+    // optFilter.put("fera", "Y")
 
-    ts_loadStart = System.currentTimeMillis()
-    // 让每个worker开始load自己的数据
-    for ((workerIdx, (start, range)) <- assignment)
-      router ! MSG_LOAD(path, workerIdx, start, range)
+    // dimension的限制条件
+    val dimFilter = mutable.HashMap[String, mutable.HashMap[String, Float]]()
+    dimFilter.put("E1", mutable.HashMap[String, Float]("min" -> 100f, "max" -> 1400f))
+
+    // 目标dimension的阈值区间
+    val dimIntervals = mutable.HashMap[String, Array[Float]]()
+    val interval_E1 = new Array[Float](10)
+    for (i <- 0 until 10)
+      interval_E1(i) = 150 * i
+    dimIntervals.put("E1", interval_E1)
+
+    val interval_E6 = new Array[Float](10)
+    for (i <- 0 until 10)
+      interval_E6(i) = 150 * i
+    dimIntervals.put("E6", interval_E6)
+
+    // 目标options
+    val targetOptions = Array[String]("care", "mb", "da", "cca", "tbs")
+    for (worker <- workers)
+      worker ! MSG_MASTER_ANALYSIS(optFilter, dimFilter, targetOptions, dimIntervals)
   }
-
-  def onLoadFinished(workerIdx: Int, elapsed: Long): Unit = {
-    println(s"worker #$workerIdx 加载数据完成, 耗时 ${elapsed/1000}秒")
-
-    workerLog.add(workerIdx)
-    if (workerLog.size == workerNum) {
-      println(s"全部的worker已经完成加载数据, 总体耗时 ${(System.currentTimeMillis() - ts_loadStart)/1000}秒")
-    }
-  }
-
 }
+
+object Master extends App {
+  val config = ConfigFactory.parseString("akka.cluster.roles=[MASTER]")
+    .withFallback(ConfigFactory.load())
+  val system = ActorSystem("CrossFilterSystem", config)
+  system.actorOf(Props(classOf[Master], args(0), args(1).toInt, args(2).toInt), name = "master")
+}
+
+
+case class MSG_MASTER_LOAD(path: String, start: Int, range: Int)
+
+case class MSG_MASTER_ANALYSIS(optFilter: mutable.HashMap[String, String],
+                               dimFilter: mutable.HashMap[String, mutable.HashMap[String, Float]],
+                               targetOptions: Array[String],
+                               dimIntervals: mutable.HashMap[String, Array[Float]])
--- akka/src/main/scala/cn/gridx/scala/akka/app/crossfilter/Worker.scala
+++ akka/src/main/scala/cn/gridx/scala/akka/app/crossfilter/Worker.scala
@@ -1,28 +1,149 @@
 package cn.gridx.scala.akka.app.crossfilter
 
-import akka.actor.Actor
+import java.util
+
+import akka.actor.{Actor, ActorPath, ActorSelection, ActorSystem, Props, RootActorPath}
+import akka.cluster.ClusterEvent.MemberUp
+import akka.cluster.{Cluster, Member}
+import com.typesafe.config.ConfigFactory
+import scopt.OptionParser
+
+import scala.collection.mutable
+
 
 /**
-  * Created by tao on 6/17/16.
+  * Created by tao on 6/19/16.
   */
 class Worker extends Actor {
+  val cluster = Cluster(context.system)
+  var master: ActorSelection = _
   val handler = new CrossFilter()
 
-  def receive = {
-    case MSG_LOAD(path, workerIdx, start, range) =>
-      println(s"Worker #$workerIdx received msg MSG_LOAD")
-      load(path, workerIdx, start, range)
+  /**
+    * 订阅系统的[MemberUp]消息
+    * */
+  override def preStart(): Unit = {
+    cluster.subscribe(self, classOf[MemberUp])
   }
+
+  override def receive = {
+    case MemberUp(member) =>
+      println(s"\n收到了消息[MemberUp], member = ${RootActorPath(member.address)} ")
+      registerSelf(member)
+    case MSG_MASTER_LOAD(path, start, range) =>
+      loadData(path, start, range)
+    case MSG_MASTER_ANALYSIS(optFilter, dimFilter, targetOptions, dimIntervals) =>
+      analyze(optFilter, dimFilter, targetOptions, dimIntervals)
+  }
+
+
+  /**
+    * 收到MASTER启动的消息后, 记录下MASTER, 并向它汇报自己
+    * */
+  def registerSelf(member: Member): Unit = {
+    if (member.hasRole("MASTER")) {
+      println(s"将向master注册自己, member = $member")
+      master = context.actorSelection(RootActorPath(member.address)/"user"/"master")
+      master ! MSG_REGISTER_WORKER
+    }
+  }
+
 
   /**
     * 每个worker加载自己的数据
     * */
-  def load(path: String, workerIdx: Int, start: Int, range: Int): Unit = {
+  def loadData(path: String, start: Int, range: Int): Unit = {
     val ts = System.currentTimeMillis()
-
     handler.LoadSourceData(path, start, range)
+    sender ! MSG_WORKER_LOAD_FINISH(System.currentTimeMillis() - ts)
+  }
 
-    sender ! MSG_WORKER_LOAD_FINISH(workerIdx, System.currentTimeMillis() - ts)
+
+
+  /**
+    * 每个worker开始分析自己持有的数据
+    * */
+  def analyze(optFilter: scala.collection.mutable.HashMap[String, String],
+              dimFilter: mutable.HashMap[String, mutable.HashMap[String, Float]],
+              targetOptions: Array[String],
+              dimIntervals: mutable.HashMap[String, Array[Float]]): Unit = {
+    println(s"${self.path} 开始分析自己的数据了")
+    val ts = System.currentTimeMillis()
+    val (dimDistribution, optDistributions) = handler.doFiltering(optFilter, dimFilter, targetOptions, dimIntervals)
+    println(s"${self.path} 分析数据完成")
+    val elapsed = System.currentTimeMillis() - ts
+
+    master ! MSG_WORKER_ANALYSIS_FINISH(self.path, elapsed, dimDistribution, optDistributions)
   }
 }
+
+case class Params(actorType: String = "unknown", actorNumber: Int = -1)
+
+
+object Worker {
+  val parser: OptionParser[Params] =
+    new scopt.OptionParser[Params]("输入节点类型, 以及节点数量") {
+      head("")
+
+      opt[String]("actorType").required().valueName("<actor type>").action{
+        (x, c) => c.copy(actorType = x)
+      } text("actor类型: seed 或者 nonSeed")
+
+      opt[Int]("actorNumber").action {
+        (x, c) => c.copy(actorNumber = x)
+      } text("actor类型: 只适用于非seed节点")
+    }
+
+
+  def main(args: Array[String]): Unit = {
+    parser.parse(args, Params()) match {
+      case Some(params) =>
+        if (params.actorType.equals("seed"))
+          startSeedActors()
+        else if (params.actorType.equals("nonSeed")) {
+          for (i <- 0 until params.actorNumber)
+            createWorkerActor(port = "0", actorName = s"Worker_$i")
+        } else
+          throw new RuntimeException("非法的`actorType`参数")
+      case _ =>
+        println("参数错误, 请重新输入")
+        parser.showUsage
+    }
+
+  }
+
+
+  /***
+    * 创建seed actor, 在一台机器上只创建一个seed actor
+    * 实际上这里的创建的seed actor自身也是一个worker actor
+    */
+  def startSeedActors(): Unit = {
+    val seedPorts = List[String]("2551")
+    for (port <- seedPorts)
+      createWorkerActor(port = port, actorName = s"Worker_$port")
+  }
+
+
+
+  /**
+    * 创建普通的worker actor
+    * */
+  def createWorkerActor(port: String, actorName: String): Unit = {
+    val config = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$port")
+      .withFallback(ConfigFactory.parseString("akk.cluster.roles=[WORKER]"))
+      .withFallback(ConfigFactory.load())
+    val system = ActorSystem("CrossFilterSystem", config)
+    system.actorOf(Props[Worker], name = actorName)
+  }
+}
+
+
+case class MSG_REGISTER_WORKER()
+
+case class MSG_WORKER_LOAD_FINISH(elapsed: Long)
+
+case class MSG_WORKER_ANALYSIS_FINISH(actorPath: ActorPath, elapsed: Long,
+                                      dimDistribution: util.HashMap[String, util.TreeMap[Int, Int]],
+                                      optDistributions: util.HashMap[String, util.HashMap[String, Int]])
+
 
