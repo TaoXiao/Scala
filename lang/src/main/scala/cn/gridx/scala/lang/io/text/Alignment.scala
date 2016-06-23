c4b53e9f3a7d3620e0ca34d4fc03de889ae131a0
tao@gridx.com <tao@gridx.com>
2016-06-23 08:18:25 +0800

    输出对齐

--- lang/src/main/scala/cn/gridx/scala/lang/io/text/Alignment.scala
+++ lang/src/main/scala/cn/gridx/scala/lang/io/text/Alignment.scala
@@ -1,11 +1,14 @@
 package cn.gridx.scala.lang.io.text
 
+import java.text.DecimalFormat
+
 /**
   * Created by tao on 6/4/16.
   */
 object Alignment {
   def main(args: Array[String]): Unit = {
     AlignString()
+    println("\n")
     AlignNumber()
   }
 
@@ -14,30 +17,32 @@ object Alignment {
     * 左对齐对齐字符串
     * 输出
     *
-      信息1 Hello world!        结束
-      信息2 Goodbye, earth !    结束
-
+    * 信息1 Hello world!        结束
+    * 信息2 Goodbye, earth !    结束
+    *
     * */
   def AlignString(): Unit = {
     val s1 = "Hello world!"
     val s2 = "Goodbye, earth !"
 
-    val res1 = String.format("%s %-20s%s", "信息1", s1, "结束")
-    val res2 = String.format("%s %-20s%s", "信息2", s2, "结束")
+    val res1 = String.format("%-40s%s", s1, "结束")
+    val res2 = String.format("%-40s%s", s2, "结束")
 
     println(res1)
     println(res2)
   }
 
 
-  /**
+  /*
     * 输出为
-      1.73
-      20.34
-      结果  1.73
-      结果 20.34
+     1.73
+     20.34
+     结果  1.73
+     结果 20.34
+     AA        123       true
+     BBBBBB    123456    false
 
-    * */
+  **/
   def AlignNumber(): Unit = {
     val n1: Float = 1.73f
     val n2: Float = 20.33985f
@@ -51,6 +56,9 @@ object Alignment {
     // 对字符串进行右对齐操作
     println(String.format("%s%6s", "结果", s1))
     println(String.format("%s%6s", "结果", s2))
+
+    println(String.format("%-10s%-10s%s", "AA",  123.toString, true.toString))
+    println(String.format("%-10s%-10s%s", "BBBBBB", 123456.toString, false.toString))
   }
 
 }
